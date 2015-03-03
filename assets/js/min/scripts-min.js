81b38aa67ef6ad77c497b203a1534d596ba0dc3d
edward7486 <enguyen@uw.edu>
2015-03-02 19:15:37 -0800

    Made site responsive
    All left: buggy menu icons still showing on rescale

--- assets/js/min/scripts-min.js
+++ assets/js/min/scripts-min.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){console.log("hello man")});
+$(document).ready(function(){$(window).width()<=1024?$("button.register").remove().prependTo(".container"):$(window).width()>=1024&&$("button.register").remove().appendTo("header"),$(window).width()<=1024?($("ul.nav").prepend("<div id='close'>&#x2715;</div>"),$("header nav").append("<div id='hamburger'>&#9776;</div>")):($("#close").remove(),$("#hamburger").remove()),$("#hamburger").on("click",function(){$("header nav ul").addClass("open")}),$("#close").on("click",function(){$("header nav ul").removeClass("open")})});
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,47 @@
 $(document).ready(function(){
 
+	// Moving Register Button in Header based on Window Width //
+	if ( $(window).width() <= 1024 ) {
+		
+		$("button.register").remove().prependTo(".container");
 
-	console.log("hello man");
+	} else if ( $(window).width() >= 1024 ) {
 
+		$("button.register").remove().appendTo("header");
+	
+	}
+
+	//
+
+	// Inserting Close button for side menu
+
+	if ( $(window).width() <= 1024 ) {
+
+		$("ul.nav").prepend("<div id='close'>&#x2715;</div>");
+
+		$("header nav").append("<div id='hamburger'>&#9776;</div>");
+
+	} else {
+
+		$("#close").remove();
+		$("#hamburger").remove();
+
+	}
+
+	//
+
+	// Menu Toggling //
+
+	$("#hamburger").on("click", function(){
+
+		$('header nav ul').addClass( 'open' );
+
+	});
+
+	$("#close").on("click", function(){
+
+		$('header nav ul').removeClass( 'open' );
+
+	});
 
 });
